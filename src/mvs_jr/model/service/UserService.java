737cc6ba9fec12b59466990d8f4862f73f63be72
AL-SOLOM-DA <ecolog.comvgorle@mail.ru>
2021-08-20 14:08:34 +0300

    Move all packages and classes in new general package "mvc_jr" Create package "dao" and package service in model's package Refresh class "Util"

---
+++ src/mvs_jr/model/service/UserService.java
@@ -0,0 +1,21 @@
+package mvs_jr.model.service;
+
+import mvs_jr.bean.User;
+
+import java.util.List;
+
+public interface UserService {
+    public User deleteUser(long id);
+
+    public User createOrUpdateUser(String name, long id, int level);
+
+    public List<User> getUserByName(String name);
+
+    public List<User> getAllDeletedUsers();
+
+    public List<User> getUsersBetweenLevels(int fromLevel, int toLevel);
+
+    List<User> filterOnlyActiveUsers(List<User> allUsers);
+
+    User getUserById(long userId);
+}
